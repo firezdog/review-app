46364d9e3f8f2dbaccb23991aed88549ae320c93
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-07-10 11:10:58 -0500

    adding database

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
 app = Flask(__name__)
+
+DATABASE = './database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if (db is None):
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+    db = get_db()
+    return render_template("index.html")
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5005, debug=False)
+    app.run(debug=True)
