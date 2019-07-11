210a736c6c4b42d4faabca40dcd53f26a87157c4
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-07-10 22:13:31 -0500

    adding sql alchemy :

---
+++ customer_manager.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(
+    os.path.join(project_dir, "customerdatabase.db")
+)
