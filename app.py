a92d1ece11b6ea8acb97fc573d85e57b5c78b4e8
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-07-10 22:13:13 -0500

    removed database for sqlalchemy

--- app.py
+++ app.py
@@ -2,30 +2,14 @@ import sqlite3
 from flask import Flask, render_template, g, request, jsonify
 app = Flask(__name__)
 
-DATABASE = './database.db'
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if (db is None):
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("index.html")
     else:
-        db = get_db()
         return jsonify(request.form)
 
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
